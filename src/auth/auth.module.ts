570736a43b193d9112a0d0a9ccef6bd11dd94d32
Christiam Alejandro Omaña Chivico <oman112360@siigo.com>
2020-05-25 21:17:21 -0500

    0.0.1 create controller auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 // import * as config from 'config';
 
-@Module({
+/* @Module({
   imports: [
     PassportModule.register({
       defaultStrategy: 'jwt',
@@ -19,31 +19,31 @@ import { JwtStrategy } from './jwt.strategy';
       },
     }),
     TypeOrmModule.forFeature([UserRepository]),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'courses',
-      synchronize: true,
-      entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    }),
   ],
-})
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtStrategy, PassportModule],
+}) */
 export class AuthModule {
-  static register(options?) {
+  static register(options: {}) {
     return {
-      // Dynamic modules must return an object with the exact same interface, plus one additional property called module.
-      // The module property serves as the name of the module, and should be the same as the class name of the module,
-      // as shown in the example below.
       module: AuthModule,
-      providers: [
-        {
-          provide: AuthService,
-          //useValue: new EasyconfigService(options),
-        },
+      imports: [
+        PassportModule.register({
+          defaultStrategy: 'jwt',
+        }),
+        JwtModule.register({
+          secret: 'courses',
+          signOptions: {
+            expiresIn: 3600,
+          },
+        }),
+        TypeOrmModule.forFeature([UserRepository]),
+        TypeOrmModule.forRoot({
+          ...options,
+          entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        }),
       ],
+      providers: [AuthService, JwtStrategy],
       exports: [AuthService, JwtStrategy, PassportModule],
     };
   }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './auth/auth.module';
 export * from './auth/auth.service';
-// export * from './auth/auth.controller';
+export * from './auth/auth.controller';
+export * from './commons/decorators/get-user.decorator';
